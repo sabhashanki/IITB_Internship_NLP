9cd7f1b5133bb6751bdb7bd4ebb6122610cc4f5c
sabhashanki <sabhashanki@gmail.com>
2022-10-22 12:54:05 +0530

    lang testing model

--- lang_detect.py
+++ lang_detect.py
@@ -2,23 +2,27 @@ import pickle
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import LabelEncoder
+import logging
+logging.basicConfig(level = logging.INFO, filename = 'lang_detect_python.log', filemode = 'w', format = '%(asctime)s - %(levelname)s - %(message)s')
 
-#importing trained model    
+# Initialization
 naive_model = pickle.load(open('naive_lang_detect_model1.pkl','rb'))
-
-#exporting csv file to retrieve y data
-df = pd.read_csv('Language Detection.csv')
-y = df.Language
-
-#label encoder for y
-le = LabelEncoder()
-encoded_y = le.fit_transform(y)
-
-#importing cleaned training data
 data = pickle.load(open('data.pkl','rb'))
-
-#vectorizing cleaned data
+le = LabelEncoder()
 cv = CountVectorizer()
+
+# exporting data 
+try:
+    df = pd.read_csv('Language Detection.csv')
+    logging.info('CSV file imported')
+except:
+    print('CSV file not found')
+    logging.error('CSV file not found')
+
+
+#label encoding and vectorization
+y = df.Language
+encoded_y = le.fit_transform(y)
 x_vector = cv.fit_transform(data).toarray()
 
 #Prediction module 
@@ -27,4 +31,3 @@ def lang_prediction(text):
     lang = naive_model.predict(x)
     lang = le.inverse_transform(lang)
     return lang[0]
-
